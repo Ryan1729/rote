2d68449912cd4f7f4992ebd53d4a719f6da766f6
Ryan1729 <Ryan1729@gmail.com>
2017-11-16 23:47:34 -0700

    Step 5
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+extern crate libc;
+
+use libc::{tcgetattr, tcsetattr, termios, ECHO, TCSAFLUSH};
 use std::io::{self, Read};
+use std::os::unix::io::AsRawFd;
+
+fn enable_raw_mode() {
+    unsafe {
+        let mut raw: termios = std::mem::uninitialized();
+        let stdin_fileno = io::stdin().as_raw_fd();
+        let raw_ptr = &mut raw as *mut termios;
+        tcgetattr(stdin_fileno, raw_ptr);
+
+        raw.c_lflag &= !ECHO;
+
+        tcsetattr(stdin_fileno, TCSAFLUSH, raw_ptr);
+    }
+}
 
 fn main() {
+    enable_raw_mode();
+
     let mut buffer = [0; 1];
     let mut stdin = io::stdin();
 
