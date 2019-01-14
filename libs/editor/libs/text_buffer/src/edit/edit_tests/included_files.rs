ade281b1da009486def9e30e5710ee0a82aa93ac
Ryan1729 <ryan1729@gmail.com>
2019-01-14 01:06:00 -0700

    reduce the line deletion test down and find the behaviour is bizarre

--- libs/editor/libs/text_buffer/src/edit/edit_tests/included_files.rs
+++ libs/editor/libs/text_buffer/src/edit/edit_tests/included_files.rs
@@ -6,16 +6,7 @@ use pretty_assertions::assert_eq;
 fn does_not_lose_a_line_in_this_manually_found_case() {
     use TestEdit::*;
     use ReplaceOrAdd::*;
-    let mut buffer = t_b!(include_str!("./testtext.txt"));
-
-    let expected_line = "            assert_eq!(u.old, e.new);";
-
-    //precondition given we assert it again below
-    {
-        let s: String = buffer.rope.clone().into();
-        assert!(s.contains(expected_line));
-    }
-    
+    let mut buffer = t_b!(include_str!("./test-text-0.txt"));
 
     buffer.set_cursor(cur!{l 21 o 0 h l 38 o 2}, Replace);
 
@@ -25,6 +16,14 @@ fn does_not_lose_a_line_in_this_manually_found_case() {
     TestEdit::apply(&mut buffer, Delete);
     TestEdit::apply(&mut buffer, Delete);
 
+    let expected_line_suffix = "assert_eq!(u.old, e.new);";
+
+    //precondition given we assert it again below
+    {
+        let s: String = buffer.rope.clone().into();
+        assert!(s.contains(expected_line_suffix), "\n*****\n\nPrecondition failure!\n\n*****\n");
+    }
+
     let line_count = buffer.rope.len_lines();
 
     buffer.set_cursor(cur!{l 92 o 0 h l 93 o 37}, Replace);
@@ -33,7 +32,77 @@ fn does_not_lose_a_line_in_this_manually_found_case() {
 
     {
         let s: String = buffer.rope.clone().into();
-        assert!(s.contains(expected_line));
+        assert!(s.contains(expected_line_suffix));
+    }
+    assert_eq!(line_count, buffer.rope.len_lines());
+}
+
+#[test]
+fn does_not_lose_a_line_in_this_reduced_manually_found_case() {
+    use TestEdit::*;
+    use ReplaceOrAdd::*;
+    let mut buffer = t_b!(include_str!("./test-text-0-reduced-0.txt"));
+
+    buffer.set_cursor(cur!{l 21 o 0 h l 38 o 2}, Replace);
+
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+
+    let expected_line_suffix = "assert_eq!(u.old, e.new);";
+
+    //precondition given we assert it again below
+    {
+        let s: String = buffer.rope.clone().into();
+        assert!(s.contains(expected_line_suffix), "\n*****\n\nPrecondition failure!\n\n*****\n");
+    }
+
+    let line_count = buffer.rope.len_lines();
+
+    buffer.set_cursor(cur!{l 92 o 0 h l 93 o 37}, Replace);
+
+    TestEdit::apply(&mut buffer, TabOut);
+
+    {
+        let s: String = buffer.rope.clone().into();
+        assert!(s.contains(expected_line_suffix));
+    }
+    assert_eq!(line_count, buffer.rope.len_lines());
+}
+
+#[test]
+fn does_not_lose_a_line_in_this_further_reduced_manually_found_case() {
+    use TestEdit::*;
+    use ReplaceOrAdd::*;
+    let mut buffer = t_b!(include_str!("./test-text-0-reduced-1.txt"));
+
+    buffer.set_cursor(cur!{l 92 o 0 h l 109 o 2}, Replace);
+
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+    TestEdit::apply(&mut buffer, Delete);
+
+    let expected_line_suffix = "assert_eq!(u.old, e.new);";
+
+    //precondition given we assert it again below
+    {
+        let s: String = buffer.rope.clone().into();
+        assert!(s.contains(expected_line_suffix), "\n*****\n\nPrecondition failure!\n\n*****\n");
+    }
+
+    let line_count = buffer.rope.len_lines();
+
+    buffer.set_cursor(cur!{l 92 o 0 h l 93 o 37}, Replace);
+
+    TestEdit::apply(&mut buffer, TabOut);
+
+    {
+        let s: String = buffer.rope.clone().into();
+        assert!(s.contains(expected_line_suffix));
     }
     assert_eq!(line_count, buffer.rope.len_lines());
 }
