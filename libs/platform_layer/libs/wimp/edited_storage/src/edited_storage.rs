1954a40539c7ca74c1d55668fbf1a6b57e5f9847
Ryan1729 <Ryan1729@gmail.com>
2019-12-04 08:25:30 -0700

    get serialize-deserialize test passing

--- libs/platform_layer/libs/wimp/edited_storage/src/edited_storage.rs
+++ libs/platform_layer/libs/wimp/edited_storage/src/edited_storage.rs
@@ -41,13 +41,62 @@ fn get_path(buffer_name: String, uuid: &u128) -> PathBuf {
     PathBuf::from(format!("{}_{}", slug, uuid))
 }
 
+const PATH_PREFIX: &'static str = "Path: ";
+const SCRATCH_PREFIX: &'static str = "Scratch: ";
+
+const UUID_SUFFIX_LENGTH: usize = 32
+    + 1 // for comma
+;
 fn serialize(name: &BufferName, uuid: u128) -> String {
-    //TODO
-    String::new()
+    use BufferName::*;
+    match name {
+        Path(p) => {
+            // if a user selects a non-unicode path, then at least this way the data will be
+            // preserved, even if it might be directed to a different filename. Excluding the
+            // corner case of two distinct paths being lossily converted to the same string,
+            // which would result in the most recent one overwriting the other one I guess?
+            // Hopefully seeing the first file being rendered with replacement characters will
+            // convince you to be careful editing files with those filenames?
+            let path_string = p.to_string_lossy();
+            format!("{}{:032x},{}", PATH_PREFIX, uuid, path_string)
+        }
+        Scratch(n) => format!("{}{:032x},{}", SCRATCH_PREFIX, uuid, n),
+    }
 }
 
-fn deserialize(s: String) -> Option<(BufferName, u128)> {
-    //TODO
+fn deserialize(s: &str) -> Option<(BufferName, u128)> {
+    if s.starts_with(PATH_PREFIX) {
+        // works becasue the prefix is ASCII
+        let s = &s[PATH_PREFIX.len()..];
+
+        if let Some((uuid, path)) = split_off_uuid_and_comma(s) {
+            return Some((BufferName::Path(PathBuf::from(path)), uuid));
+        }
+    } else if s.starts_with(SCRATCH_PREFIX) {
+        // works becasue the prefix is ASCII
+        let s = &s[SCRATCH_PREFIX.len()..];
+        if let Some((uuid, rest)) = split_off_uuid_and_comma(s) {
+            if let Ok(n) = rest.parse() {
+                return Some((BufferName::Scratch(n), uuid));
+            }
+        }
+    }
+
+    None
+}
+
+fn split_off_uuid_and_comma<'s>(s: &'s str) -> Option<(u128, &'s str)> {
+    // > not >= to exclude "" as a `rest`!
+    if s.chars().count() > UUID_SUFFIX_LENGTH {
+        let (uuid_and_comma, rest) = &s.split_at(UUID_SUFFIX_LENGTH);
+        if uuid_and_comma.ends_with(",") {
+            if let Ok(uuid) = u128::from_str_radix(&uuid_and_comma[..(UUID_SUFFIX_LENGTH - 1)], 16)
+            {
+                return Some((uuid, rest));
+            }
+        }
+    }
+
     None
 }
 
@@ -59,8 +108,26 @@ mod tests {
     pub fn buffer_name() -> impl Strategy<Value = BufferName> {
         prop_oneof![
             any::<u32>().prop_map(BufferName::Scratch),
-            any::<std::ffi::OsString>().prop_map(|s| BufferName::Path(PathBuf::from(s)))
+            any::<std::ffi::OsString>().prop_map(|s| {
+                let s = if s.len() == 0 {
+                    "empty-paths-are-invalid".into()
+                } else {
+                    s
+                };
+                BufferName::Path(PathBuf::from(s))
+            })
         ]
+    }
+
+    fn serialize_then_deserialize_works_on(name: BufferName, uuid: u128) {
+        let serialized = serialize(&name, uuid);
+        let deserialized = deserialize(&serialized);
+        let expected = Some((name, uuid));
+        assert_eq!(
+            deserialized, expected,
+            "{:?} deserialized into \"{:?}\", not \"{:?}\"",
+            serialized, deserialized, expected
+        )
     }
 
     proptest! {
@@ -69,7 +136,7 @@ mod tests {
             name in buffer_name(),
             uuid in any::<u128>(),
         ) {
-            assert_eq!(deserialize(serialize(&name, uuid)), Some((name, uuid)))
+            serialize_then_deserialize_works_on(name, uuid)
         }
     }
 }
