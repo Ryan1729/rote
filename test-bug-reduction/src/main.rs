bb00f874e73c14ba71bc5d9af5ef5e5e96ca1d3c
Ryan1729 <ryan1729@gmail.com>
2020-05-07 01:07:00 -0600

    remove some easy to remove pieces

--- test-bug-reduction/src/main.rs
+++ test-bug-reduction/src/main.rs
@@ -1,24 +1,2 @@
-use macros::d;
-use platform_types::{Input, UpdateAndRenderOutput};
 
-fn update_and_render(input: Input) -> UpdateAndRenderOutput {
-    use lazy_static::lazy_static;
-    lazy_static! {
-        static ref STATE_MUTEX: std::sync::Mutex<editor::State> =
-            std::sync::Mutex::new(editor::new());
-    }
-    match STATE_MUTEX.try_lock() {
-        Ok(mut state) => editor::update_and_render(&mut state, input),
-        Err(e) => {
-            if cfg!(feature = "invariant-checking") {
-                panic!("STATE_MUTEX already borrowed!? \n{}", e);
-            }
-            d!()
-        }
-    }
-}
-
-fn main() {
-    println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-    platform_layer::run(update_and_render);
-}
+fn main() {}
--- test-bug-reduction/text/basic_parse_test.rs
+++
@@ -1,12 +0,0 @@
-struct Ab {
-    a: &'static str,
-    b: u8,
-}
-
-fn main() {
-    let hi = "hi there";
-    // TODO
-}
-/*
-fn uncommentable_function() {
-    let _ = "foo/*.rs"; //  */}*/
