3d7d042f6eec35b8fa2eaec4529b7313b5802a5e
Ryan1729 <Ryan1729@gmail.com>
2020-08-11 01:06:00 -0600

    merge platform_types arb into tests without breaking repro

--- libs/platform_types/src/tests.rs
+++ libs/platform_types/src/tests.rs
@@ -4,7 +4,27 @@ use screen_space::*;
 use crate::screen_positioning::{
     MapElements,
     ScreenSpaceWH,
-};
+};
+
+use proptest::prelude::{Strategy};
+use pub_arb_abs::{abs_pos, abs_length};
+
+pub fn scroll_xy() -> impl Strategy<Value = ScrollXY> {
+    let spec = abs_pos();
+    (spec, spec).prop_map(|(x, y)| slxy!{ x, y })
+}
+
+pub fn wh() -> impl Strategy<Value = ScreenSpaceWH> {
+    let strat = abs_length();
+    (strat, strat).prop_map(|(w, h)| ScreenSpaceWH { w, h })
+}
+
+pub fn scrollable_screen() -> impl Strategy<Value = ScrollableScreen> {
+    (scroll_xy(), wh()).prop_map(|(scroll, wh)| ScrollableScreen {
+        scroll,
+        wh,
+    })
+}
 // It turns out this is also needed only in the tests and in fact, it's kind of in the way since we now
 // want to store the pieces separately. I'll leave the original comments here for posterity.
 
@@ -22,5 +42,3 @@ impl MapElements<abs::Pos> for ScrollableScreen {
         }
     }
 }
-
-pub mod arb;
--- libs/platform_types/src/tests/arb.rs
+++ libs/platform_types/src/tests/arb.rs
@@ -1,21 +1,3 @@
 use super::*;
-use proptest::prelude::{Strategy};
-use pub_arb_abs::{abs_pos, abs_length};
 
-pub fn scroll_xy() -> impl Strategy<Value = ScrollXY> {
-    let spec = abs_pos();
-    (spec, spec).prop_map(|(x, y)| slxy!{ x, y })
-}
-
-pub fn wh() -> impl Strategy<Value = ScreenSpaceWH> {
-    let strat = abs_length();
-    (strat, strat).prop_map(|(w, h)| ScreenSpaceWH { w, h })
-}
-
-pub fn scrollable_screen() -> impl Strategy<Value = ScrollableScreen> {
-    (scroll_xy(), wh()).prop_map(|(scroll, wh)| ScrollableScreen {
-        scroll,
-        wh,
-    })
-}
 
