4b4b93561f0b2f7c258cf268a4ef5b71000efc69
Ryan1729 <Ryan1729@gmail.com>
2020-08-11 01:06:00 -0600

    delete platform layer without breaking repro

--- libs/platform_layer/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// We might have different platform layer options later, so let's keep this separate.
-use platform_types::UpdateAndRender;
-pub fn run(update_and_render: UpdateAndRender) {
-    let result = wimp::run(update_and_render);
-
-    if let Err(e) = result {
-        println!("wimp::run(update_and_render) error:\n{}", e);
-    }
-}
