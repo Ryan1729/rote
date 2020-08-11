6f0fb5e65d6701cb16c97a0702fc14a3c3ee5a91
Ryan1729 <Ryan1729@gmail.com>
2020-08-11 01:06:00 -0600

    remove wh without breaking repro

--- libs/platform_types/src/platform_types.rs
+++ libs/platform_types/src/platform_types.rs
@@ -2,14 +2,12 @@
 
 #[cfg(any(test, feature = "pub_arb"))]
 pub mod tests{
-use screen_space::*;
 use proptest::prelude::{Strategy};
-use pub_arb_abs::{abs_pos, abs_length};
+use pub_arb_abs::{abs_pos};
 
 #[derive(Default, Debug)]
 pub struct ScrollableScreen {
     pub scroll: ScrollXY,
-    pub wh: (),
 }
 
 pub fn scroll_xy() -> impl Strategy<Value = ScrollXY> {
@@ -17,15 +15,9 @@ pub fn scroll_xy() -> impl Strategy<Value = ScrollXY> {
     (spec, spec).prop_map(|(x, y)| slxy!{ x, y })
 }
 
-pub fn wh() -> impl Strategy<Value = ()> {
-    let strat = abs_length();
-    (strat, strat).prop_map(|(_w, _h)| ())
-}
-
 pub fn scrollable_screen() -> impl Strategy<Value = ScrollableScreen> {
-    (scroll_xy(), wh()).prop_map(|(scroll, wh)| ScrollableScreen {
-        scroll,
-        wh,
+    scroll_xy().prop_map(|scroll| ScrollableScreen {
+        scroll
     })
 }
 }
