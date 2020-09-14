e0faaaf856709d8e9a879250ad5553b2616b96e5
Ryan1729 <Ryan1729@gmail.com>
2020-09-14 01:06:00 -0600

    create scaffolding to add ab_glyph backend feature

--- libs/platform_layer/libs/wimp/gl_layer/libs/text_rendering/libs/glyph-brush/libs/rasterizer/src/rasterizer.rs
+++ libs/platform_layer/libs/wimp/gl_layer/libs/text_rendering/libs/glyph-brush/libs/rasterizer/src/rasterizer.rs
@@ -1,6 +1,20 @@
+#[cfg(not(any(feature = "rusttype", feature = "ab_glyph")))]
+compile_error!("Either feature \"rusttype\" or \"ab_glyph\" must be enabled for this crate.");
+
+#[cfg(feature = "rusttype")]
 pub use rusttype::{
-    Error, Font, Glyph, GlyphId, HMetrics, Point, PositionedGlyph,
-    Rect, Scale, ScaledGlyph, Vector, VMetrics,
-    point, vector,
+    Font, Glyph, GlyphId, HMetrics, Point, PositionedGlyph,
+    Rect, Scale, ScaledGlyph, VMetrics,
+    point,
 };
+#[cfg(feature = "rusttype")]
 pub use rusttype::gpu_cache::{Cache, CachedBy};
+
+#[cfg(feature = "ab_glyph")]
+pub use ab_glyph::{ // TODO use real ab_glyph exports
+    Font, Glyph, GlyphId, HMetrics, Point, PositionedGlyph,
+    Rect, Scale, ScaledGlyph, VMetrics,
+    point,
+};
+#[cfg(feature = "ab_glyph")]
+pub use ab_glyph::gpu_cache::{Cache, CachedBy};
