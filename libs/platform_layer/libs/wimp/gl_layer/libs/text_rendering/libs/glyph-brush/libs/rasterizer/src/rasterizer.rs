a34c579b2840289e974370bc7b0fa37df1b3a692
Ryan1729 <Ryan1729@gmail.com>
2020-09-15 01:06:00 -0600

    start trying to make rusttype and glyph_brush_draw_cache each work separately

--- libs/platform_layer/libs/wimp/gl_layer/libs/text_rendering/libs/glyph-brush/libs/rasterizer/src/rasterizer.rs
+++ libs/platform_layer/libs/wimp/gl_layer/libs/text_rendering/libs/glyph-brush/libs/rasterizer/src/rasterizer.rs
@@ -1,20 +1,20 @@
-#[cfg(not(any(feature = "rusttype", feature = "ab_glyph")))]
-compile_error!("Either feature \"rusttype\" or \"ab_glyph\" must be enabled for this crate.");
+#[cfg(not(any(feature = "rusttype", feature = "glyph_brush_draw_cache")))]
+compile_error!("Either feature \"rusttype\" or \"glyph_brush_draw_cache\" must be enabled for this crate.");
 
 #[cfg(feature = "rusttype")]
 pub use rusttype::{
     Font, Glyph, GlyphId, HMetrics, Point, PositionedGlyph,
     Rect, Scale, ScaledGlyph, VMetrics,
     point,
+    gpu_cache::{Cache, CachedBy},
 };
-#[cfg(feature = "rusttype")]
-pub use rusttype::gpu_cache::{Cache, CachedBy};
 
-#[cfg(feature = "ab_glyph")]
-pub use ab_glyph::{ // TODO use real ab_glyph exports
-    Font, Glyph, GlyphId, HMetrics, Point, PositionedGlyph,
-    Rect, Scale, ScaledGlyph, VMetrics,
-    point,
+#[cfg(feature = "glyph_brush_draw_cache")]
+pub use glyph_brush_draw_cache::{
+    ab_glyph::{
+        Font, Glyph, GlyphId, HMetrics, Point, PositionedGlyph,
+        Rect, PxScale as Scale, ScaledGlyph, VMetrics,
+        point,
+    },
+    DrawCache as Cache, CachedBy
 };
-#[cfg(feature = "ab_glyph")]
-pub use ab_glyph::gpu_cache::{Cache, CachedBy};
