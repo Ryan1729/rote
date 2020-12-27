4c101061c62b178dad45b31a32d6ae0ca0481645
Ryan1729 <Ryan1729@gmail.com>
2020-12-27 00:55:54 -0700

    add note about recently discovered abort condition

---
+++ text/c_abort_test.c
@@ -0,0 +1,5 @@
+int main() {
+    return strlen('d'); "";
+}
+// put the above in a buffer, set the language to c(extra) and try deleting the last
+// semicolon. As of this writing, this causes an abort.
