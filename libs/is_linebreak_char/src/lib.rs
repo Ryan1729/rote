6422735876048edc6a890e0f6bde1b7a09abcfd4
Ryan1729 <Ryan1729@gmail.com>
2022-04-30 01:06:00 -0600

    address clippy::manual_range_contains warning

--- libs/is_linebreak_char/src/lib.rs
+++ libs/is_linebreak_char/src/lib.rs
@@ -2,7 +2,7 @@
 pub fn is_linebreak_char(c: char) -> bool {
     // ropey treats these as line breaks, so we do too.
     // See also https://www.unicode.org/reports/tr14/tr14-32.html
-    (c >= '\u{a}' && c <= '\r')
+    ('\u{a}'..='\r').contains(&c)
         || c == '\u{0085}'
         || c == '\u{2028}'
         || c == '\u{2029}'
