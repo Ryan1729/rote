16c9bcc1d82bb7d8515360b2dc12ff55a906f052
Ryan1729 <Ryan1729@gmail.com>
2022-05-04 01:06:00 -0600

    use a manually vendored version of `gl` crate. Note that it builds.

---
+++ libs/platform_layer/libs/wimp/gl_layer/libs/gl/examples/basic.rs
@@ -0,0 +1,51 @@
+// Copyright 2015 Brendan Zabarauskas and the gl-rs developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate gl;
+extern crate glutin;
+
+fn main() {
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let window = glutin::window::WindowBuilder::new();
+    let gl_window = glutin::ContextBuilder::new()
+        .build_windowed(window, &event_loop)
+        .unwrap();
+
+    // It is essential to make the context current before calling `gl::load_with`.
+    let gl_window = unsafe { gl_window.make_current().unwrap() };
+
+    // Load the OpenGL function pointers
+    gl::load_with(|symbol| gl_window.get_proc_address(symbol));
+
+    event_loop.run(move |event, _, control_flow| {
+        use glutin::event::{Event, WindowEvent};
+        use glutin::event_loop::ControlFlow;
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+                    gl::Clear(gl::COLOR_BUFFER_BIT);
+                }
+                gl_window.swap_buffers().unwrap();
+            },
+            _ => (),
+        }
+    });
+}
