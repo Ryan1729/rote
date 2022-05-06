5568c8ba9fee6c5412ef4a676f125cb1d1b9c667
Ryan1729 <Ryan1729@gmail.com>
2022-05-06 01:06:00 -0600

    reduce `compile-error-minimization.rs` down further, keeping error

--- libs/platform_layer/libs/wimp/gl_layer/examples/compile-error-minimization.rs
+++ libs/platform_layer/libs/wimp/gl_layer/examples/compile-error-minimization.rs
@@ -1,79 +1,11 @@
 /// This is meant to be the smallest reasonable example, and perhaps a template
 /// for other examples/applications.
 use glutin_wrapper::{Api, GlProfile, GlRequest};
-use platform_types::{screen_positioning::*};
 use shared::Res;
-use macros::{d};
 
-pub use gl_layer_types::{TextLayout, TextOrRect, TextSpec, VisualSpec};
-
-use platform_types::{CharDim};
-
-pub struct State<'font> {
-    open_gl: open_gl::State,
-    text_rendering: text_rendering::State<'font>,
-}
-
-macros::fmt_debug!(for State<'_>: _ in "{}", "State");
-
-pub use text_rendering::FONT_LICENSE;
-
-pub fn get_char_dims(state: &State, text_sizes: &[f32]) -> Vec<CharDim> {
-    state.text_rendering.get_char_dims(text_sizes)
-}
-
-pub fn set_dimensions(state: &mut State, hidpi_factor: f32, wh: (i32, i32)) {
-    state.open_gl.set_dimensions(wh);
-
-    state.text_rendering.set_dimensions(hidpi_factor);
-}
-
-#[perf_viz::record]
-pub fn render(
-    state: &mut State,
-    text_or_rects: &[TextOrRect],
-    width: u32,
-    height: u32,
-) -> Res<()> {
-    state.open_gl.begin_frame();
-
-    let resize_texture = |new_width: u32, new_height: u32| {
-        eprint!("\r                            \r");
-        eprintln!("Resizing glyph texture -> {}x{}", new_width, new_height);
-
-        open_gl::State::resize_texture(new_width, new_height);
-    };
-
-    let replacement_vertices = state.text_rendering.render_vertices(
-        &text_or_rects,
-        (width, height),
-        |rect: text_rendering::TextureRect, tex_data: &_| {
-            // Update part of gpu texture with new glyph alpha values
-            open_gl::State::update_texture(
-                rect.min.x as _,
-                rect.min.y as _,
-                rect.width() as _,
-                rect.height() as _,
-                tex_data,
-            );
-        },
-        resize_texture,
-    );
-
-    if let Some(vertices) = replacement_vertices {
-        perf_viz::record_guard!("open_gl.draw_vertices");
-        state.open_gl.draw_vertices(vertices);
-    }
-
-    state.open_gl.end_frame();
-
-    Ok(())
-}
-
-pub fn cleanup(
-    state: &State,
-) -> Res<()> {
-    state.open_gl.cleanup()
+pub fn takes_dyn(
+    _: &dyn Fn(),
+) {
 }
 
 fn main() -> Res<()> {
@@ -94,150 +26,29 @@ fn main() -> Res<()> {
         )?;
     let glutin_wrapper_context = unsafe { glutin_wrapper_context.make_current().map_err(|(_, e)| e)? };
 
-    const TEXT_SIZE: f32 = 16.0;
+    type LoadFn = dyn Fn();
+    let load_fn: &LoadFn = &|| { &glutin_wrapper_context; };
 
-    let mut hidpi_factor = 1.0;
-
-    let mut gl_state = {
-        let text_rendering_state = text_rendering::new(
-            hidpi_factor as f32
-        )?;
-    
-        type LoadFnOutput = *const core::ffi::c_void;
-        
-        let load_fn: &dyn Fn(*const u8) -> LoadFnOutput = &|symbol| {
-            // SAFETY: The underlying library has promised to pass us a nul 
-            // terminated pointer.
-            let cstr = unsafe { std::ffi::CStr::from_ptr(symbol as _) };
-    
-            let s = cstr.to_str().unwrap();
-    
-            glutin_wrapper_context.get_proc_address(s) as _
-        };
-
-        // Load the OpenGL function pointers
-        // SAFETY: The passed load_fn must always return accurate function pointer 
-        // values, or null on failure.
-        unsafe { open_gl::load_global_gl(load_fn); }
-
-        let open_gl = open_gl::State::new_already_loaded(
-            [0.3, 0.3, 0.3, 1.0],
-            text_rendering_state.texture_dimensions(),
-        )?;
-        State {
-            open_gl,
-            text_rendering: text_rendering_state,
-        }
-    };
-
-    let mut loop_helper = spin_sleep::LoopHelper::builder()
-        .report_interval_s(1./500.)
-        .build_with_target_rate(250.0);
-
-    let mut running = true;
-    let mut dimensions = glutin_wrapper_context
-        .window()
-        .inner_size();
-
-    let text_colour = [0.9, 0.9, 0.9, 1.0];
-
-    let mut frame_count: u32 = 0;
+    takes_dyn(load_fn);
 
     {
         events.run(move |event, _, control_flow| {
             use glutin_wrapper::event::*;
 
             match event {
-                Event::MainEventsCleared if running => {
+                Event::MainEventsCleared => {
                     // Queue a RedrawRequested event so we draw the updated view quickly.
                     glutin_wrapper_context.window().request_redraw();
-                }
-                Event::RedrawRequested(_) => {
-                    frame_count = frame_count.wrapping_add(1);
-                    let width = dimensions.width as f32;
-                    let height = dimensions.height as f32;
-
-                    let mut text_and_rects = Vec::with_capacity(16);
-
-                    let rate = loop_helper.report_rate().unwrap_or_default();
-
-                    let text = &format!(
-                        "Hello world! {rate:.2} FPS on frame {frame_count}"
-                    );
-
-                    text_and_rects.push(TextOrRect::Text(TextSpec {
-                        text,
-                        size: TEXT_SIZE,
-                        spec: VisualSpec {
-                            rect: ScreenSpaceRect {
-                                min: ssxy!(0.0, height - TEXT_SIZE),
-                                max: ssxy!(width, height),
-                            },
-                            colour: text_colour,
-                            ..d!()
-                        },
-                        layout: TextLayout::Unbounded,
-                    }));
-
-                    render(&mut gl_state, &text_and_rects, width as _, height as _)
-                        .expect("render didn't work");
-
-                    glutin_wrapper_context
-                        .swap_buffers()
-                        .expect("swap_buffers didn't work!");
-                    loop_helper.loop_sleep();
-
-                    // We want to track the time that the message loop takes too!
-                    loop_helper.loop_start();
-                }
-                Event::NewEvents(StartCause::Init) => {
-                    // At least try to measure the first frame accurately
-                    loop_helper.loop_start();
                 }
                 Event::WindowEvent { event, .. } => {
                     macro_rules! quit {
                         () => {{
-                            running = false;
-
-                            let _ = cleanup(&gl_state);
-
                             *control_flow = glutin_wrapper::event_loop::ControlFlow::Exit;
                         }};
                     }
 
                     match event {
                         WindowEvent::CloseRequested => quit!(),
-                        WindowEvent::ScaleFactorChanged {
-                            scale_factor,
-                            ..
-                        } => {
-                            hidpi_factor = scale_factor;
-                        }
-                        WindowEvent::Resized(size) => {
-                            glutin_wrapper_context.resize(size);
-                            dimensions = size;
-                            set_dimensions(
-                                &mut gl_state,
-                                hidpi_factor as _,
-                                (dimensions.width as _, dimensions.height as _),
-                            );
-                        }
-                        WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(keypress),
-                                    ..
-                                },
-                            ..
-                        }=> {
-                            match keypress {
-                                VirtualKeyCode::Escape => {
-                                    quit!();
-                                },
-                                _ => (),
-                            };
-                        }
                         _ => {}
                     }
                 }
