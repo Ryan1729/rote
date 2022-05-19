2a7c462d2cd544dac562913c81790abacad68f07
Ryan1729 <Ryan1729@gmail.com>
2022-05-19 01:06:00 -0600

    switch to `rfd` since `nfd` a) crashes sometimes, and b) is unmaintained

--- libs/platform_layer/libs/file_chooser/src/file_chooser.rs
+++ libs/platform_layer/libs/file_chooser/src/file_chooser.rs
@@ -1,35 +1,28 @@
 use std::path::{PathBuf};
 
 macro_rules! open_dialog {
-    ($nfd_func: ident($filter_list: expr, $default_path: expr), $callback: ident) => {
+    ($rfd_method: ident($default_path: expr), $callback: ident) => {
         let join_handle = std::thread::Builder::new()
             .name("file chooser".to_string())
             .spawn(move || {
-                let filter_list = $filter_list;
-                let default_path: Option<&str> = $default_path
+                let default_path: Option<_> = $default_path
                     .as_mut()
-                    .and_then(|p: &mut PathBuf| {
+                    .map(|p: &mut PathBuf| {
                         if p.is_file() {
                             p.pop();
                         }
-                        p.to_str()
+                        p
                     });
 
-                match nfd::$nfd_func(
-                    filter_list,
-                    default_path,
-                ) {
-                    Ok(nfd::Response::Okay(s)) => $callback(s.into()),
-                    Ok(nfd::Response::OkayMultiple(v)) => {
-                        for s in v {
-                            $callback(s.into());
-                            break;
-                        }
-                    }
-                    Ok(nfd::Response::Cancel) => {}
-                    Err(e) => {
-                        eprintln!("{}", e);
-                    }
+                let mut dialog = rfd::FileDialog::new();
+
+                if let Some(p) = default_path {
+                    dialog = dialog.set_directory(p);
+                }
+
+                match dialog.$rfd_method() {
+                    Some(s) => $callback(s.into()),
+                    None => {},
                 };
             });
 
@@ -46,7 +39,7 @@ pub fn single<F>(mut default_path: Option<PathBuf>, callback: F)
 where
     F: Fn(PathBuf) + Send + Sync + 'static,
 {
-    open_dialog!(open_file_dialog(None, default_path), callback);
+    open_dialog!(pick_file(default_path), callback);
 }
 
 /// Presents a file save dialog to the user. If the user selects a file then the callback will be
@@ -56,5 +49,5 @@ pub fn save<F>(mut default_path: Option<PathBuf>, callback: F)
 where
     F: Fn(PathBuf) + Send + Sync + 'static,
 {
-    open_dialog!(open_save_dialog(None, default_path), callback);
+    open_dialog!(save_file(default_path), callback);
 }
